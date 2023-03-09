ce810637cb5c27329730909d538c8799448559fe
olsib8 <106234333+olsib8@users.noreply.github.com>
2023-03-09 20:06:44 +0000

    Deploying to gh-pages from @ olsib8/goit-js-hw-09@f42a97ef311e20ed64d29f45fba1c218a1a46342 🚀

--- 01-color-switcher.38128ccc.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=document.querySelector("body"),o=null,c=!1;t.addEventListener("click",(function(){c||(c=!0,o=setInterval((function(){e.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3))})),n.addEventListener("click",(function(){c=!1,clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.38128ccc.js.map
